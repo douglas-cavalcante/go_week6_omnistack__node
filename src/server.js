dc9a68654e4da8f28e3a586e75b49cc9b971741c
douglas-cavalcante <henrique.douglas.costaa@gmail.com>
2019-04-13 19:58:14 -0300

    adicionar socket.io

--- src/server.js
+++ src/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+app.user(cors());
+
+//config requisocões WS
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  console.log("ok");
+  socket.on('connectRoom', box => {
+    socket.join(box);
+  });
+})
+
 
 mongoose.connect('mongodb+srv://douglas:testeteste123@cluster0-fgak6.mongodb.net/test?retryWrites=true',
   {
@@ -10,10 +25,15 @@ mongoose.connect('mongodb+srv://douglas:testeteste123@cluster0-fgak6.mongodb.net
   },
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+  return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
